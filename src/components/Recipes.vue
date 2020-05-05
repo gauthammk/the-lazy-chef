bd6f87d556a962e66eea017e9533ae4dc6e6cb25
gauthammk <gr8gautham@gmail.com>
2020-05-05 23:22:36 +0530

    fixed href on the recipes page

--- src/components/Recipes.vue
+++ src/components/Recipes.vue
@@ -4,12 +4,7 @@
     <div v-bind:key="recipe.id" v-for="recipe in recipes">
       <v-container class="col-6">
         <v-card>
-          <v-card-title>
-            {{ recipe.title }}
-            <v-btn class="mx-2" fab dark small color="green">
-              <v-icon>mdi-open-in-new</v-icon>
-            </v-btn>
-          </v-card-title>
+          <v-card-title>{{ recipe.title }}</v-card-title>
           <v-card-text>
             <v-chip-group>
               <v-chip
@@ -17,6 +12,9 @@
                 :key="ingredient"
               >{{ ingredient }}</v-chip>
             </v-chip-group>
+            <v-btn class="mx-2" fab dark small color="green" :href="recipe.href">
+              <v-icon>mdi-open-in-new</v-icon>
+            </v-btn>
           </v-card-text>
 
           <!-- <p v-for="ingredient in recipe.ingredients.split(',')" :key="ingredient">{{ ingredient }}</p> -->
