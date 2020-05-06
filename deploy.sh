a591f9ed4ad3d59cc0fb6578286799955446e023
gauthammk <gr8gautham@gmail.com>
2020-05-06 15:50:56 +0530

    added deploy script

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:gauthammk/vue-recipe-app.git master:gh-pages
+git push -f git@github.com:gauthammk/the-lazy-chef.git master:gh-pages
 
 cd -
